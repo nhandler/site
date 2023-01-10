4d25db9ea6ea2f9e51770de6f5b93b1c85ecf98c
anandani4136 <anandani4136@students.d211.org>
2023-01-09 23:05:42 -0600

    :bug: Fixed static height :sparkles: added MoonLight

--- src/pages/Registration/Background/index.tsx
+++ src/pages/Registration/Background/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-import KITCHEN from 'assets/registration/kitchen_background.svg';
+import MOONLIGHT from 'assets/registration/carnival_background.svg';
 import styles from './styles.module.scss';
 
 const Background: React.FC = () => (
   <div className={styles.background}>
-    <div className={styles.kitchen} style={{ backgroundImage: `url("${KITCHEN}")` }} />
+    <div className={styles.kitchen} style={{ backgroundImage: `url("${MOONLIGHT}")` }} />
   </div>
 );
 
