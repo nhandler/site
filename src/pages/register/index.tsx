5bd6e56471afa7a650656338409edb1db38784e0
Aniket Gargya <gargya.aniket@gmail.com>
2023-01-17 11:06:12 -0600

    :truck: move registration to /register route

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -14,18 +14,18 @@ const Registration: React.FC = () => {
 
   const router = useRouter();
 
-  // useEffect(() => {
-  //   if (!isAuthenticated()) {
-  //     authenticate(`${process.env.NEXT_PUBLIC_REACT_APP_URL}${router.pathname}`);
-  //   }
-  // }, []);
   useEffect(() => {
-    setTimeout(() => {
-        if (!isAuthenticated()) {
-          authenticate(`${process.env.NEXT_PUBLIC_REACT_APP_URL}${router.pathname}`);
-        }
-    }, 5000);
-      }, []);
+    if (!isAuthenticated()) {
+      authenticate(`${process.env.NEXT_PUBLIC_REACT_APP_URL}${router.pathname}`);
+    }
+  }, []);
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     if (!isAuthenticated()) {
+  //       authenticate(`${process.env.NEXT_PUBLIC_REACT_APP_URL}${router.pathname}`);
+  //     }
+  //   }, 5000);
+  // }, []);
 
   return (
     <div className={styles.registration}>
