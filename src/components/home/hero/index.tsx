1e5ed779e5a4b9020b2550c38ec676863d787a3f
anandani4136 <ra7353@hotmail.com>
2023-01-22 22:15:30 -0600

    :lipstick: Home Screen Register Button

--- src/components/home/hero/index.tsx
+++ src/components/home/hero/index.tsx
@@ -4,12 +4,17 @@ import FERRIS_WHEEL_LARGE from 'assets/home/hero/ferris_wheel_large.svg';
 import FERRIS_WHEEL_SMALL from 'assets/home/hero/ferris_wheel_small.svg';
 import BANNER from 'assets/home/hero/banner.svg';
 import LINES from 'assets/home/hero/lines.svg';
-
+import router, { useRouter } from 'next/router';
+import Button from 'components/form/Button';
 import styles from './styles.module.scss';
+import buttonStyles from '../../../components/form/Button/styles.module.scss'
 
 const Section: React.FC = () => (
   <section className={styles.hero}>
     <div className={styles.heroBackground}>
+      <div className={styles.leftAlign}>
+        <button className={buttonStyles.button} type="button" onClick={() => router.push('/register')}>Register</button>
+      </div>
       <div className={styles.topHeader}>
         <h2>Welcome to</h2>
         <h1>HackIllinois</h1>
