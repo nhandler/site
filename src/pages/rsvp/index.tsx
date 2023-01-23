57a5e6b41ecb81bb15a53965732c8b9a16bd05d4
Aniket Gargya <gargya.aniket@gmail.com>
2023-01-23 13:01:28 -0600

    :building_construction: reroute /rsvp to /register

--- src/pages/rsvp/index.tsx
+++ src/pages/rsvp/index.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import Background from '../../components/register/background';
 import styles from './styles.module.scss';
 import Form from '../../components/rsvp/form';
 
-const RSVP = (): JSX.Element => (
-  <div className={styles.rsvp}>
-    <Background />
-    <Form />
-  </div>
-);
+import { useRouter } from 'next/router';
+
+const RSVP = (): JSX.Element => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace('/register');
+  }, []);
+
+  return (
+    <div className={styles.rsvp}>
+      <Background />
+      <Form />
+    </div>
+  );
+};
 
 export default RSVP;
