806d61c6139d5d74ea139a86328a831a3d1cd713
anandani4136 <ra7353@hotmail.com>
2023-01-25 22:14:57 -0600

    :art: Updating the FAQ Text

--- src/components/faqs/faqs.tsx
+++ src/components/faqs/faqs.tsx
@@ -2,42 +2,36 @@ import React from 'react';
 
  const faqs = [
    {
-     sectionTitle: 'During',
+     sectionTitle: 'General',
      sectionFaqs: [
+       {
+          question: 'When and where is HackIllinois?',
+          answer: [
+            <p key={0}>HackIllinois will be held in-person from February 24-26th on UIUC campus.</p>,
+          ],
+       },
+       {
+        question: 'How do I participate?',
+        answer: [
+          <p key={0}>You must apply in order to participate in HackIllinois. We have an Early Application deadline due on February 3rd and Late Application deadline due February 12th. Admissions will be based on a weighted lottery, and applying early will increase your chances of admission. Decisions will be released shortly after both application deadlines, and if you’re accepted, there will be a limited time to RSVP before we reallocate your spot to another student.</p>,
+        ],
+     },
        {
          question: 'How do I get to the University of Illinois?',
          answer: [
-           <p key={0}>HackIllinois will be providing some bus routes to the event. If you are a University of Illinois student, please walk or utilize the MTD bus system.</p>,
-         ],
-       },
-       {
-         question: 'When and where is HackIllinois?',
-         answer: [
-           <p key={0}>HackIllinois will be held from February 25-27th virtually on a Discord Server through which attendees can ask questions and interact with one another.</p>,
-         ],
-       },
-       {
-         question: 'How do applications work?',
-         answer: [
-           <p key={0}>You must apply in order to be considered for HackIllinois. Admissions will be based on a weighted lottery. Decisions will be released in late January in multiple waves, and it is to your advantage to apply as soon as possible. If you’re accepted, there will be a limited time to RSVP before we reallocate your spot to another student.</p>,
+           <p key={0}>HackIllinois will be providing a bus for students traveling from West Lafayette. If you are a University of Illinois student, please walk or utilize the MTD bus system.</p>,
          ],
        },
        {
          question: 'How can I stay updated with what is going on at the event?',
          answer: [
-           <p key={0}>Please download the HackIllinois app (available shortly before the event on the App Store and Play Store)! Our website will also be continually updated with new information throughout the year. We will also be sending out reminders for various workshops and events on our Discord. Once you register, you’ll receive instructions on how to RSVP and join the Discord server.</p>,
+           <p key={0}>Please download the HackIllinois app (available shortly before the event on the App Store and Play Store)! Our website will also be continually updated with new information throughout the year. We will also be sending out reminders for various workshops and events through our mobile apps and Discord.</p>,
          ],
        },
        {
-         question: 'Have more questions?',
+         question: 'Do I need to have a programming background to participate?',
          answer: [
-           <p key={0}>Please don’t hesitate to reach out to us at <a href="mailto:contact@hackillinois.org">contact@hackillinois.org</a> if you have any other questions.</p>,
-         ],
-       },
-       {
-         question: 'Interested in being a sponsor at our event?',
-         answer: [
-           <p key={0}>Email us at <a href="mailto:sponsor@hackillinois.org">sponsor@hackillinois.org</a>!</p>,
+           <p key={0}>No! People of all skill levels are welcomed.</p>,
          ],
        },
      ],
@@ -46,58 +40,66 @@ import React from 'react';
      sectionTitle: 'Before',
      sectionFaqs: [
        {
-         question: 'Do I need a team? How do I find one?',
-         answer: [
-           <p key={0}>No, you are not required to have a team to participate. With that being said, we strongly encourage you to work with other participants in order to get the full experience at HackIllinois. You can make teams prior to the event or find members through our team matching event (As part of Hackathons Crash Course/Hackathons 101) or on the Discord.</p>,
-         ],
-       },
-       {
          question: 'What should I bring?',
          answer: [
            <p key={0}>You should bring a student ID, reusable water bottle, change of clothing, personal items such as toiletries, laptop, and charger. Due to safety considerations, please do not bring desktop computers, extra monitors, weapons, or alcoholic beverages.</p>,
          ],
        },
        {
+         question: 'Is there anything to do other than code?',
+         answer: [
+           <p key={0}>Absolutely! There will be a bunch of fun mini-events this year, including a Shark Tank competition, keynotes, and opportunities to connect with company representatives. Check out the full schedule when it is posted on our site!</p>,
+         ],
+       },
+       
+       {
          question: 'Will there be food?',
          answer: [
-           <p key={0}>Unfortunately, we won’t be able to provide meals or meal vouchers as our event is fully virtual.</p>,
+           <p key={0}>Snacks, drinks, and all meals will be provided for the entire weekend. If you have a dietary restriction, please make sure to mention it when you register. We’ll have a wide variety of food available throughout the weekend, including vegetarian and vegan options. We will use our mobile apps to make announcements when food arrives.</p>,
          ],
        },
        {
-         question: 'Do I need to have a programming background to participate?',
+         question: 'Where do I sleep?',
          answer: [
-           <p key={0}>No! People of all skill levels are welcomed!</p>,
+           <p key={0}>HackIllinois won’t be able to provide sleeping rooms, so you must find separate lodging on-campus or off-campus.</p>,
          ],
        },
-       {
-         question: 'Is there a Code of Conduct?',
-         answer: [
-           <p key={0}>Yes! All attendees of the event (sponsors, mentors, etc.) agree to the <u>HackIllinois 2022 Code of Conduct</u> </p>,
-         ],
-       },
+       
      ],
    },
    {
-     sectionTitle: 'After',
+     sectionTitle: 'During',
      sectionFaqs: [
        {
-         question: 'Is there anything to do other than code?',
+         question: 'Do I need a team? How do I find one?',
          answer: [
-           <p key={0}>Yes! There will be a variety of mini-events, workshops, and company events for attendees to join. You should definitely check out the schedule that will be posted here on the website!</p>,
+           <p key={0}>No, you are not required to have a team to participate. You are encouraged to work with mentors and other participants in order to get the full experience. If you do have a team, please keep it to a maximum of 4 people.</p>,
          ],
        },
        {
-         question: 'How can I stay updated with what is going on at the event?',
+         question: 'Do I have to go to the opening/ending ceremony?',
          answer: [
-           <p key={0}>Please download the HackIllinois app (available shortly before the event on the App Store and Play Store)! Our website will also be continually updated with new information throughout the year. We will also be sending out reminders for various workshops and events on our Discord. Once you register, you’ll receive instructions on how to RSVP and join the Discord server.</p>,
+           <p key={0}>Yes! Swag, prize, and food information will be covered at the opening ceremony. Prizes will be announced at the ending ceremony.</p>,
          ],
        },
        {
-         question: 'Have more questions?',
+         question: 'Can I work on my own projects?',
          answer: [
-           <p key={0}>Please don’t hesitate to reach out to us at <a href="mailto:contact@hackillinois.org">contact@hackillinois.org</a> if you have any other questions!</p>,
+           <p key={0}>Yes, feel free to work on your own project, people in past years have won creating projects from scratch! However, the benefit of working on a HackIllinois project track is that you get guidance and the opportunity to communicate with experts in the field.</p>,
          ],
        },
+       {
+        question: 'What facilities, floors, and rooms are available to work in?',
+        answer: [
+          <p key={0}>Working space will be available in Siebel Computer Science and CIF.</p>,
+        ],
+      },
+      {
+        question: 'How can I stay updated with what is going on at the event?',
+        answer: [
+          <p key={0}>Please download our mobile apps beforehand! Our website will also be continually updated with new information throughout the year.</p>,
+        ],
+      },
      ],
    },
  ];
