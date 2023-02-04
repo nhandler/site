4f5cfed0e577490c5d1d874ee913839f7a2ce124
Aniket Gargya <gargya.aniket@gmail.com>
2023-02-04 13:42:29 -0600

    :lipstick: small ui changes on landing page

--- src/components/home/hero/index.tsx
+++ src/components/home/hero/index.tsx
@@ -10,23 +10,22 @@ import styles from './styles.module.scss';
 const Section: React.FC = () => (
   <section className={styles.hero}>
     <div className={styles.heroBackground}>
-      <nav>
-        <img className={styles.logo} src={LOGO} />
-        <ul>
-          <li><a href="/register">Register</a></li>
-        </ul>
-      </nav>
-
       <div className={styles.topHeader}>
+        <nav>
+          <img className={styles.logo} src={LOGO} />
+          <ul>
+            <li><a href="/register">Register</a></li>
+          </ul>
+        </nav>
         <div className={styles.welcomeTo}>Welcome to</div>
         <h1>HackIllinois</h1>
+        <div className={styles.dates}>Feb 24th - Feb 26th</div>
       </div>
       <img className={styles.ferrisWheelLarge} src={FERRIS_WHEEL_LARGE} />
       <img className={styles.ferrisWheelSmall} src={FERRIS_WHEEL_SMALL} />
     </div>
     <div className={styles.heroFooter}>
       <span className={styles.subtitle}>making memories</span>
-      <span className={styles.subsubtitle}>February 24 - 26</span>
       <img className={styles.banner} src={BANNER} />
       <img className={styles.lines} src={LINES} />
     </div>
