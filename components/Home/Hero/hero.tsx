201791a68377776f83c3a9a66e7281b5ab82d503
Nick King <nrking0@gmail.com>
2023-10-04 21:38:30 -0500

    Boilerplate template

---
+++ components/Home/Hero/hero.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import styles from "./styles.module.scss";
+
+
+const Hero: React.FC = () => {
+
+    return(
+        <div className={styles.heroMain}>
+            Hello!
+        </div>
+    );
+
+};
+
+export default Hero;
