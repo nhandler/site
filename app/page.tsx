50883706657e9b39775ab2bb9dfbdf75fe7e3ba2
Jish2 <j.j.jappiecraft@gmail.com>
2023-10-24 14:14:13 -0500

    ⚙️ add event component

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import styles from "./page.module.scss";
 import Hero from "@/components/Home/Hero/hero";
+import Events from "@/components/Home/Events/events";
 
 const Home: React.FC = () => {
     return (
         <main className={styles.main}>
-            <Hero/>
+            <Hero />
+            <Events />
         </main>
     );
-}
+};
 
-export default Home; 
+export default Home;
---
+++ components/Home/Events/events.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+import styles from "./styles.module.scss";
+
+import Image from "next/image";
+
+const Hero: FC = () => {
+    return <section className={styles.eventsMain}></section>;
+};
+
+export default Hero;
