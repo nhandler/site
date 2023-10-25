0bd1b3b0966f0aa6bf9f335925186975d49e1753
Sarena Yang <sarenay2@illinois.edu>
2023-10-25 00:12:57 -0500

    basic faq comp created, fixing responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import styles from "./page.module.scss";
 import Hero from "@/components/Home/Hero/hero";
+import Faq from "@/components/Home/Faq/Faq";
 
 const Home: React.FC = () => {
     return (
         <main className={styles.main}>
             <Hero/>
+            <Faq/>
         </main>
     );
 }
---
+++ components/Home/Faq/Faq.tsx
@@ -0,0 +1,92 @@
+"use client";
+
+import React from "react";
+import { useState } from "react";
+
+import styles from "./styles.module.scss";
+
+import Image from "next/image";
+
+import faqs from "./questions";
+
+import FaqHeader from "@/public/home/faq/faq.svg";
+import FaqCloud from "@/public/home/faq/right cloud.svg";
+import ClickedSword from "@/public/home/faq/clickedSword.svg";
+import Sword from "@/public/home/faq/sword.svg";
+import Flames from "@/public/home/faq/faqFlames.svg";
+
+const Faq: React.FC = () => {
+
+    const [faqSectionIndex, setFaqSectionIndex] = useState(0);
+    const [generalClicked, setGeneralClicked] = useState(true);
+    const [beforeClicked, setBeforeClicked] = useState(false);
+    const [duringClicked, setDuringClicked] = useState(false);
+
+    const swordStates = [generalClicked, beforeClicked, duringClicked];
+    const handleClick = (id: string) => {
+        if (id === "general") {
+            setFaqSectionIndex(0);
+            setGeneralClicked(true);
+            setBeforeClicked(false);
+            setDuringClicked(false);
+        } else if (id === "before") {
+            setFaqSectionIndex(1);
+            setGeneralClicked(false);
+            setBeforeClicked(true);
+            setDuringClicked(false);
+        } else {
+            setFaqSectionIndex(2);
+            setGeneralClicked(false);
+            setBeforeClicked(false);
+            setDuringClicked(true);
+        }
+    }
+    return (
+        <section className={styles.faq}>
+            <div className={styles.faqContainer}>
+                <div className={styles.faqContainerItem1}>
+                    <Image className={styles.faqHeader} alt="faq header" src={FaqHeader} />
+                    <Image className={styles.faqCloud} alt="faq cloud" src={FaqCloud} />
+                </div>
+                <div className={styles.faqContainerItem2}>
+                    <h2>General</h2>
+                    <Image 
+                        className={styles.swords} 
+                        alt="general sword" 
+                        onClick={() => handleClick("general")}
+                        src={generalClicked ? ClickedSword : Sword} />
+                    <h2>Before</h2>
+                    <Image 
+                        className={styles.swords} 
+                        alt="before sword" 
+                        onClick={() => handleClick("before")}
+                        src={beforeClicked ? ClickedSword : Sword} />
+
+                    <h2>During</h2>
+                    <Image 
+                        className={styles.swords} 
+                        alt="during sword"
+                        onClick={() => handleClick("during")}
+                        src={duringClicked ? ClickedSword : Sword} />
+
+
+                    
+                    <Image className={styles.faqFlames} alt="faq flames" src={Flames} />
+                </div>
+                <div className={styles.faqContainerItem3}>
+                    <div className={styles.faqContent}>
+                        {faqs[faqSectionIndex].sectionFaqs.map(({ question, answer }, i) => (
+                        <div key={i} className={styles.faqContentColumn}>
+                            <h3>{question}</h3>
+                            {answer}
+                        </div>
+                        ))}
+                    </div>
+                </div>
+                
+            </div>
+        </section>
+    );
+};
+
+export default Faq;
