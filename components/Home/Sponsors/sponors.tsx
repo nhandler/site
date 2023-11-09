3db987b7a4ae6317cd5c377e2f167fd10b1fb5bf
Nick King <nrking0@gmail.com>
2023-11-08 23:40:56 -0600

    Add Icon, sponsor, and clouds

--- components/Home/Sponsors/sponors.tsx
+++ components/Home/Sponsors/sponors.tsx
@@ -5,24 +5,34 @@ import Lottie from "lottie-react";
 import crystalBall from "@/public/home/sponsors/crystalball.json";
 import Image from "next/image";
 import background from "@/public/home/sponsors/background.svg";
+import clouds from "@/public/home/sponsors/clouds.svg";
 
 const Sponsors: React.FC = () => {
     return (
-        <div className={styles.sponsorsMain}>
-            <div className={styles.sponsorsBackground}>
-                <h1 className={styles.sponsorsTitle}>Our Sponsors</h1>
-                <Image
-                    className={styles.backgroundSVG}
-                    alt="background"
-                    src={background}
+        <section className={styles.sponsorsContainer}>
+            <div className={styles.sponsorsMain}>
+                <div className={styles.sponsorsBackground}>
+                    <h1 className={styles.sponsorsTitle}>Our Sponsors</h1>
+                    <Image
+                        className={styles.backgroundSVG}
+                        alt="background"
+                        src={background}
+                    />
+                </div>
+                <Lottie
+                    className={styles.ball}
+                    animationData={crystalBall}
+                    loop={true}
                 />
             </div>
-            <Lottie
-                className={styles.ball}
-                animationData={crystalBall}
-                loop={true}
-            />
-        </div>
+            <div className={styles.cloudsWrapper}>
+                <Image
+                    className={styles.backgroundClouds}
+                    alt="clouds background"
+                    src={clouds}
+                />
+            </div>
+        </section>
     );
 };
 
