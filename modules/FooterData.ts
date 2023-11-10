6f5dbcdb37749e88fa0308ffa3b863363cdc28f8
Jish2 <j.j.jappiecraft@gmail.com>
2023-11-09 19:01:03 -0600

    update footer links

--- modules/FooterData.ts
+++ modules/FooterData.ts
@@ -1,33 +1,33 @@
-export const data = [
+export const socials_data = [
     {
         id: 1,
         name: "Facebook",
         image: "/footer/facebook.svg",
-        url: "https://www.facebook.com"
+        url: "https://www.facebook.com/hackillinois/"
     },
     {
         id: 2,
         name: "Instagram",
         image: "/footer/instagram.svg",
-        url: "https://www.instagram.com"
+        url: "https://www.instagram.com/HackIllinois"
     },
     {
         id: 3,
         name: "X",
         image: "/footer/twitter.svg",
-        url: "https://www.twitter.com"
+        url: "https://www.twitter.com/HackIllinois"
     },
     {
         id: 4,
         name: "LinkedIn",
         image: "/footer/linkedin.svg",
-        url: "https://www.linkedin.com"
+        url: "https://www.linkedin.com/company/hackillinois"
     },
     {
         id: 5,
         name: "Github",
         image: "/footer/github.svg",
-        url: "https://www.github.com"
+        url: "https://www.github.com/HackIllinois"
     },
     {
         id: 6,
@@ -35,4 +35,4 @@ export const data = [
         image: "/footer/email.svg",
         url: "mailto:contact@hackillinois.org"
     }
-]
+];
