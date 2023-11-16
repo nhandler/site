a7dc155926e6664ba2d82e21ef1cfe7420aa3803
Joshua Goon <25201575+Jish2@users.noreply.github.com>
2023-11-16 17:32:44 -0600

    ↕️ Footer restyle (#127)
    * ❎ css reset
    
    * ↕️ footer sizing changes
    
    * 💬 remove comment
    
    * 🤦 adjustments
    
    * Slight change to sizing
    
    ---------
    
    Co-authored-by: Nick King <nrking0@gmail.com>

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -20,29 +20,31 @@ const Footer: React.FC<FooterProps> = ({
     newsletterTitle = "Sign up for our newsletter to get new updates!"
 }) => {
     return (
-        <div className={styles.footer}>
-            <div id="social-section">
-                <span className={styles.title}>{title}</span>
-                <div className={styles.icons}>
-                    {socials.map((icon: FooterIcon) => (
-                        <a href={icon.url} key={icon.id} target="_blank">
-                            <div className={styles.icon}>
-                                {/* eslint-disable-next-line */}
-                                <img
-                                    src={icon.image}
-                                    alt={icon.name}
-                                    className={styles.image}
-                                />
-                            </div>
-                        </a>
-                    ))}
+        <div className={styles.footerContainer}>
+            <div className={styles.footer}>
+                <div id="social-section">
+                    <span className={styles.title}>{title}</span>
+                    <div className={styles.icons}>
+                        {socials.map((icon: FooterIcon) => (
+                            <a href={icon.url} key={icon.id} target="_blank">
+                                <div className={styles.icon}>
+                                    {/* eslint-disable-next-line */}
+                                    <img
+                                        src={icon.image}
+                                        alt={icon.name}
+                                        className={styles.image}
+                                    />
+                                </div>
+                            </a>
+                        ))}
+                    </div>
                 </div>
-            </div>
-            <div id="newsletter-section">
-                <span className={styles.newsletterTitle}>
-                    {newsletterTitle}
-                </span>
-                <Subscribe />
+                <div id="newsletter-section">
+                    <span className={styles.newsletterTitle}>
+                        {newsletterTitle}
+                    </span>
+                    <Subscribe />
+                </div>
             </div>
         </div>
     );
