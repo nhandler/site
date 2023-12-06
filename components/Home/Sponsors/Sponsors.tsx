3b279baa6ac31b7f64aad46c4f728818ea5f20ce
Tanmoy Debnath <53893576+tandebnath@users.noreply.github.com>
2023-12-06 01:24:38 -0600

    Sponsors work in progress

--- components/Home/Sponsors/Sponsors.tsx
+++ components/Home/Sponsors/Sponsors.tsx
@@ -9,6 +9,19 @@ const Sponsors: React.FC = () => {
                 <img src="/home/sponsors/background.svg" />
             </div>
             <div className={styles.art}>
+                <div className={styles.shelf}>
+                    <img src="/home/sponsors/potions/bottle-sponsors.svg" />
+                    <img src="/home/sponsors/potions/mushroom-plant.svg" />
+                    <img src="/home/sponsors/potions/lamp.svg" />
+                </div>
+                <div className={styles.potions}>
+                <img src="/home/sponsors/potions/bottle-red.svg" />
+                <img src="/home/sponsors/potions/bottle-green.svg" />
+                <img src="/home/sponsors/potions/bottle-orange.svg" />
+                <img src="/home/sponsors/potions/bottle-blue.svg" />
+                <img src="/home/sponsors/potions/bottle-yellow.svg" />
+                <img src="/home/sponsors/potions/bottle-purple.svg" />
+                </div>
                 <div className={styles.gizmo}>
                     <img src="/home/sponsors/gizmo.svg" />
                 </div>
