47c086a0b3386a269d544c3d81bdd6dd5cac3b14
Sarena Yang <sarenay2@illinois.edu>
2023-12-06 02:09:34 -0600

    added text, need to add button functionality and make responsive

--- components/Home/New_Faq/NewFAQ.tsx
+++ components/Home/New_Faq/NewFAQ.tsx
@@ -1,6 +1,6 @@
 "use client"
 import styles from './styles.module.scss'
-import Book from '@/public/home/new_faq/bookwfeather.svg'
+import Book from '@/public/home/new_faq/book (4).svg'
 import FAQHeader from '@/public/home/new_faq/faq header.svg'
 import faqs from "../../../modules/FaqData";
 import Image from 'next/image'
@@ -8,7 +8,6 @@ import { useState } from 'react';
 
 const NewFAQ = () => {
     const [faqSectionIndex, setFaqSectionIndex] = useState(0);
-    // const swordStates = [generalClicked, beforeClicked, duringClicked];
     return ( 
         <section className={styles.faq}>
             <div className={styles.faqContainer}>
@@ -42,24 +41,24 @@ const NewFAQ = () => {
                     
         
                     <Image src={Book} alt="Book" className={styles.faqBook} />
-
-                    {/* <div className={styles.faqContent}>
-                        {faqs[faqSectionIndex].sectionFaqs.map(
-                            ({ question, answer }, i) => (
-                                <div
-                                    key={i}
-                                    className={styles.faqContentColumn}
-                                >
-                                    <h3>{question}</h3>
-                                    {answer}
-                                </div>
-                            )
-                        )}
-                    </div> */}
+                    
+                    <div className={styles.faqTextContainer}>
+                        <div className={styles.faqContent}>
+                            {faqs[faqSectionIndex].sectionFaqs.map(
+                                ({ question, answer }, i) => (
+                                    <div
+                                        key={i}
+                                        className={styles.faqContentColumn}
+                                    >
+                                        <h3>{question}</h3>
+                                        {answer}
+                                    </div>
+                                )
+                            )}
+                        </div>
+                    </div>
+                    
                 </div>
-
-                
-
 
             </div>
         </section>
