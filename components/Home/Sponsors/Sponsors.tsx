faaed039daba3368edc7dfc15e19b343206ca72b
Tanmoy Debnath <53893576+tandebnath@users.noreply.github.com>
2023-12-06 01:11:34 -0600

    Sponsors work in progress

--- components/Home/Sponsors/Sponsors.tsx
+++ components/Home/Sponsors/Sponsors.tsx
@@ -1,23 +1,16 @@
 "use client";
 import React from "react";
 import styles from "./Sponsors.module.scss";
-import Lottie from "lottie-react";
-import crystalBall from "@/public/home/sponsors/crystalball.json";
-import Image from "next/image";
-import background from "@/public/home/sponsors/background.svg";
-import clouds from "@/public/home/sponsors/clouds.svg";
 
 const Sponsors: React.FC = () => {
     return (
         <section className={styles.container}>
-            <div className={styles.sponsorsMain}>
-                <div className={styles.sponsorsBackground}>
-                    <h1 className={styles.sponsorsTitle}>Our Sponsors</h1>
-                    {/* <Image
-                        className={styles.backgroundSVG}
-                        alt="background"
-                        src={background}
-                    /> */}
+            <div className={styles.background}>
+                <img src="/home/sponsors/background.svg" />
+            </div>
+            <div className={styles.art}>
+                <div className={styles.gizmo}>
+                    <img src="/home/sponsors/gizmo.svg" />
                 </div>
             </div>
         </section>
