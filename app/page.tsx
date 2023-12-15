6bcbae338928f58624f475bfcb43698e17f2a5e7
Nick King <68927798+nrking0@users.noreply.github.com>
2023-12-15 11:54:52 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Hero from "@/components/Home/Hero/hero";
 import Faq from "@/components/Home/Faq/Faq";
 import Events from "@/components/Home/Events/events";
 import JoinUs from "@/components/Home/JoinUs/JoinUs";
-import Sponsors from "@/components/Home/Sponsors";
+import Sponsors from "@/components/Home/Sponsors/Sponsors";
 import Footer from "@/components/Footer/Footer";
 import HackKnights from "@/components/Home/HackKnights/HackKnights";
 
