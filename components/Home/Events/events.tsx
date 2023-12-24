63f087a56e5619f5dcf2d7dc53b0d3ce4033870c
Jish2 <j.j.jappiecraft@gmail.com>
2023-12-23 22:09:16 -0600

    🎪 update event component

--- components/Home/Events/events.tsx
+++ components/Home/Events/events.tsx
@@ -2,126 +2,75 @@ import { FC } from "react";
 
 import styles from "./styles.module.scss";
 
+import LeftPages from "@/public/home/events/left-pages.svg";
+import RightPages from "@/public/home/events/right-pages.svg";
+import TopRightFold from "@/public/home/events/top-right-fold.svg";
+import TopLeftFold from "@/public/home/events/top-left-fold.svg";
+
 import Image from "next/image";
 
-import BrFold from "@/public/home/events/br-fold.svg";
-import TlFold from "@/public/home/events/tl-fold.svg";
-import TrFold from "@/public/home/events/tr-fold-modified.svg";
-import Nail from "@/public/home/events/nail.svg";
-import Butterfly from "@/public/home/events/butterfly.svg";
-import Caterpillar from "@/public/home/events/caterpillar.svg";
-import Ladybug from "@/public/home/events/ladybug.svg";
 import { event_description } from "@/modules/EventsData";
+import { Bushes, Plank } from "@/components/Home/Events/event-svgs";
 
 const Hero: FC = () => {
     return (
         <section className={styles.eventsMain}>
             <div className={styles.topPlankWrapper}>
-                <Image
-                    alt="bottom plank"
-                    src="/home/events/wood-plank.png"
-                    fill={true}
-                    className={styles.plankImage}
-                />
-                <div className={styles.leftNail}>
-                    <Image
-                        alt="nail"
-                        src={Nail}
-                        fill={true}
-                        aria-hidden="true"
-                    />
-                </div>
-                <div className={styles.rightNail}>
-                    <Image
-                        alt="nail"
-                        src={Nail}
-                        fill={true}
-                        aria-hidden="true"
-                    />
-                </div>
+                <Plank />
             </div>
 
-            <div className={styles.sheet}>
-                <h1 className={styles.eventsLabel}>{"The Event"}</h1>
-                <p className={styles.eventsDescription}>
-                    {event_description.description_1}
-                </p>
-                <p className={styles.eventsDescription}>
-                    {event_description.description_2}
-                </p>
-                <div className={styles.brFoldWrapper}>
+            <div className={styles.leftPageWrapper}>
+                <Image
+                    alt="left pages"
+                    src={LeftPages}
+                    width={283}
+                    height={442}
+                />
+            </div>
+            <div className={styles.rightPageWrapper}>
+                <Image
+                    alt="right pages"
+                    src={RightPages}
+                    width={254}
+                    height={532}
+                />
+            </div>
+
+            <div className={styles.sheetWrapper}>
+                <div className={styles.topRightFoldWrapper}>
                     <Image
-                        alt="fold"
-                        src={BrFold}
-                        fill={true}
-                        aria-hidden="true"
+                        alt="top right fold"
+                        aria-hidden
+                        src={TopRightFold}
+                        width={407}
+                        height={168}
                     />
                 </div>
-                <div className={styles.tlFoldWrapper}>
+                <div className={styles.topLeftFoldWrapper}>
                     <Image
-                        alt="fold"
-                        src={TlFold}
-                        fill={true}
-                        aria-hidden="true"
+                        alt="top left fold"
+                        aria-hidden
+                        src={TopLeftFold}
+                        width={73}
+                        height={68}
                     />
                 </div>
-                <div className={styles.trFoldWrapper}>
-                    <Image
-                        alt="fold"
-                        src={TrFold}
-                        fill={true}
-                        aria-hidden="true"
-                    />
-                </div>
-                <div className={styles.butterflyWrapper}>
-                    <Image
-                        alt="butterfly"
-                        src={Butterfly}
-                        fill={true}
-                        aria-hidden="true"
-                    />
-                </div>
-                <div className={styles.ladybugWrapper}>
-                    <Image
-                        alt="ladybug"
-                        src={Ladybug}
-                        fill={true}
-                        aria-hidden="true"
-                    />
+                <div className={styles.sheet}>
+                    <h1 className={styles.eventsLabel}>{"The Event"}</h1>
+                    <p className={styles.eventsDescription}>
+                        {event_description.description_1}
+                    </p>
+                    <p className={styles.eventsDescription}>
+                        {event_description.description_2}
+                    </p>
+                    <p className={styles.eventsTagline}>
+                        <i>{event_description.description_3}</i>
+                    </p>
                 </div>
             </div>
 
             <div className={styles.bottomPlankWrapper}>
-                <Image
-                    alt="bottom plank"
-                    src="/home/events/wood-plank.png"
-                    fill={true}
-                    className={styles.plankImage}
-                />
-                <div className={styles.leftNail}>
-                    <Image
-                        alt="nail"
-                        src={Nail}
-                        fill={true}
-                        aria-hidden="true"
-                    />
-                </div>
-                <div className={styles.rightNail}>
-                    <Image
-                        alt="nail"
-                        src={Nail}
-                        fill={true}
-                        aria-hidden="true"
-                    />
-                </div>
-                <div className={styles.caterpillarWrapper}>
-                    <Image
-                        alt="caterpillar"
-                        src={Caterpillar}
-                        fill={true}
-                        aria-hidden="true"
-                    />
-                </div>
+                <Bushes />
             </div>
         </section>
     );
