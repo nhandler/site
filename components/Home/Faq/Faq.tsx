f26b3837a1d5b8a3a356a53be475da9e57bf88b2
Sarena Yang <sarenay2@illinois.edu>
2024-01-02 00:24:09 -0600

    fixed desktop book functionality

--- components/Home/Faq/Faq.tsx
+++ components/Home/Faq/Faq.tsx
@@ -4,100 +4,106 @@ import styles from './styles.module.scss'
 import FAQHeader from '@/public/home/faq/faq header.svg'
 import Image from 'next/image'
 
-import BefDur from '@/public/home/faq/faq-lotties/Bef-Dur_Spellbook.json'
-import BefGen from '@/public/home/faq/faq-lotties/Bef-Gen_Spellbook.json'
-import DurGen from '@/public/home/faq/faq-lotties/Dur-Gen_Spellbook.json'
-import DurBef from "@/public/home/faq/faq-lotties/Dur-Bef_Spellbook.json"
-import GenBef from '@/public/home/faq/faq-lotties/Gen-Bef_Spellbook.json'
-import GenDur from '@/public/home/faq/faq-lotties/Gen-Dur_Spellbook.json'
+import { BefDur, BefGen, DurGen, DurBef, GenBef, GenDur } from '@/public/home/faq/faq-lotties-spellbook/index'
+import { BefDur2, BefGen2, DurGen2, DurBef2, GenBef2, GenDur2 } from '@/public/home/faq/faq-lotties-scroll/index'
 
 import { useState, useEffect } from 'react';
 import Lottie from 'lottie-react';
 
 const FAQ = () => {
-    const [clicked, setClicked] = useState(0);
-    const [initialDisplay, setInitialDisplay] = useState(true);
-    const [gbDisplay, setGbDisplay] = useState(false);
-    const [gdDisplay, setGdDisplay] = useState(false);
-
-    const [bdDisplay, setBdDisplay] = useState(false);
-    const [bgDisplay, setBgDisplay] = useState(false);
-
-    const [dgDisplay, setDgDisplay] = useState(false);
-    const [dbDisplay, setDbDisplay] = useState(false);
-
-    const handleClick = (id: number) => {
-        
-        console.log("handling click for tab " + id);
-        if (initialDisplay) {
-            if (id == 1) {
-                setGbDisplay(true);
-                setInitialDisplay(false);
-            } else if (id == 2) {
-                setGdDisplay(true);
-                setInitialDisplay(false);
-            }
-        }
-        if (gbDisplay) {
-            if (id == 2) {
-                setBdDisplay(true);
-                setGbDisplay(false);
-            } else if (id == 0) {
-                setBgDisplay(true);
-                setGbDisplay(false);
-            }
-        } else if (gdDisplay) {
-            if (id == 1) {
-                setDbDisplay(true);
-                setGdDisplay(false);
-            } else if (id == 0) {
-                setDgDisplay(true);
-                setGdDisplay(false);
-            }
-        } else if (bdDisplay) {
-            if (id == 0) {
-                setDgDisplay(true);
-                setBdDisplay(false);
-            } else if (id == 1) {
-                setDbDisplay(true);
-                setBdDisplay(false);
-            }
-        } else if (bgDisplay) {
-            if (id == 1) {
-                setGbDisplay(true);
-                setBgDisplay(false);
-            } else if (id == 2) {
-                setGdDisplay(true);
-                setBgDisplay(false);
-            }
-        } else if (dgDisplay) {
-            if (id == 1) {
-                setGbDisplay(true);
-                setDgDisplay(false);
-            } else if (id == 2) {
-                setGdDisplay(true);
-                setDgDisplay(false);
-            }
-        } else if (dbDisplay) {
-            if (id == 0) {
-                setBgDisplay(true);
-                setDbDisplay(false);
-            } else if (id == 2) {
-                setBdDisplay(true);
-                setDbDisplay(false);
-            }
+    const isMobile = () => {
+        if (typeof window !== "undefined") {
+            console.log("window width: " + window.innerWidth)
+            return window.innerWidth <= 768;
         }
     }
+
+    // const [mobile, setMobile] = useState(isMobile());
+
+    // const usebd = mobile ? BefDur2 : BefDur;
+    // const usebg = mobile ? BefGen2 : BefGen;
+    // const usedg = mobile ? DurGen2 : DurGen;
+    // const usedb = mobile ? DurBef2 : DurBef;
+    // const usegb = mobile ? GenBef2 : GenBef;
+    // const usegd = mobile ? GenDur2 : GenDur;
+
+    const usebd = BefDur2;
+    const usebg = BefGen2;
+    const usedg = DurGen2;
+    const usedb = DurBef2;
+    const usegb = GenBef2;
+    const usegd = GenDur2;
+
+    // const usebd = BefDur;
+    // const usebg = BefGen;
+    // const usedg = DurGen;
+    // const usedb = DurBef;
+    // const usegb = GenBef;
+    // const usegd = GenDur;
+    
+    const [display, setDisplay] = useState(usedg);
+
+    const handleClick = (id: number) => {
+        console.log("handling click for tab " + id);
+        if (display == usegb) {
+            if (id == 2) {
+                setDisplay(usebd);
+            } else if (id == 0) {
+                setDisplay(usebg);
+            }
+        } else if (display == usegd) {
+            if (id == 1) {
+                setDisplay(usedb);
+            } else if (id == 0) {
+                setDisplay(usedg);
+            }
+        } else if (display == usebd) {
+            if (id == 0) {
+                setDisplay(usedg);
+            } else if (id == 1) {
+                setDisplay(usedb);
+            }
+        } else if (display == usebg) {
+            if (id == 1) {
+                setDisplay(usegb);
+            } else if (id == 2) {
+                setDisplay(usegd);
+            }
+        } else if (display == usedg) {
+            if (id == 1) {
+                setDisplay(usegb);
+            } else if (id == 2) {
+                setDisplay(usegd);
+            }
+        } else if (display == usedb) {
+            if (id == 0) {
+                setDisplay(usebg);
+            } else if (id == 2) {
+                setDisplay(usebd);
+            }
+        } 
+    }
+
     useEffect(() => {
         const general = document.getElementById("general");
-        general?.addEventListener('click', () => handleClick(0));
         const before = document.getElementById("before");
-        before?.addEventListener('click', () => handleClick(1));
         const during = document.getElementById("during");
-        during?.addEventListener('click', () => handleClick(2));
-        setClicked(clicked + 1);
 
-    }, [clicked]);
+        const handleGeneralClick = () => handleClick(0);
+        const handleBeforeClick = () => handleClick(1);
+        const handleDuringClick = () => handleClick(2);
+
+        general?.addEventListener('click', handleGeneralClick);
+        before?.addEventListener('click', handleBeforeClick);
+        during?.addEventListener('click', handleDuringClick);
+        
+        return () => {
+            general?.removeEventListener('click', handleGeneralClick);
+            before?.removeEventListener('click', handleBeforeClick);
+            during?.removeEventListener('click', handleDuringClick);
+        }
+
+    }, [display]);
         
     return ( 
         <section className={styles.faq}>
@@ -107,42 +113,13 @@ const FAQ = () => {
                 </div>
             
                 <div className={styles.faqLotties}>
-                    { initialDisplay && <Lottie 
-                        animationData={GenBef}
-                        loop={false}
-                        autoplay={false}
-                    /> }
-                    { gbDisplay && <Lottie 
-                        animationData={GenBef}
+                    { <Lottie 
+                        animationData={display}
                         loop={false}
                         autoplay={true}
-                    /> }
-                    { gdDisplay && <Lottie 
-                        animationData={GenDur}
-                        loop={false}
-                        autoplay={true}
-                    /> }
-                    { bdDisplay && <Lottie 
-                        animationData={BefDur}
-                        loop={false}
-                        autoplay={true}
-                        /> }
-                    { bgDisplay && <Lottie
-                        animationData={BefGen}
-                        loop={false}
-                        autoplay={true}
-                    /> }
-                    { dgDisplay && <Lottie 
-                        animationData={DurGen}
-                        loop={false}
-                        autoplay={true}
-                    /> }
-                    { dbDisplay && <Lottie 
-                        animationData={DurBef}
-                        loop={false}
-                        autoplay={true}
-                    /> }
-
+                    />
+                    }
+                    
                 </div>
             </div>
         </section>
---
+++ public/home/faq/faq-lotties-spellbook/index.js
@@ -0,0 +1,6 @@
+export const BefDur = require('./Bef-Dur_Spellbook.json');
+export const BefGen = require('./Bef-Gen_Spellbook.json');
+export const DurBef = require('./Dur-Bef_Spellbook.json');
+export const DurGen = require('./Dur-Gen_Spellbook.json');
+export const GenBef = require('./Gen-Bef_Spellbook.json');
+export const GenDur = require('./Gen-Dur_Spellbook.json');
